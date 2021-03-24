9a68797ccc2a29da0b4c81d79564484ea49275bf
Aina <ainaard@mail.ru>
2021-03-24 21:43:31 +0300

    fixed all work

--- src/main/resources/indexes.py
+++ src/main/resources/indexes.py
@@ -1,33 +1,60 @@
-import glob
+from pymorphy2 import MorphAnalyzer
 
 path = "C:/AinaArd/Crawler/src/main/resources/crawler4j/"
 dict = {}
+morph = MorphAnalyzer()
+search = "воспоминанию делаем блины"
 
-fileLemmas = open('C:/AinaArd/Crawler/src/main/resources/lemmas.txt', 'r', encoding='utf-8')
-lemmas = []
-
-for line in fileLemmas:
+fileTokens = open('C:/AinaArd/Crawler/src/main/resources/tokens.txt', 'r', encoding='utf-8')
+tokens = []
+for line in fileTokens:
     line = line.split(" ")
-    lemmas.append(line[0])
+    tokens.append(line)
 
 
-def countAllLemmaApperances(filePath, lemma, i):
-    file = open(filePath, encoding='utf-8')
-    for line in file:
-        if line.count(lemma):
-            if lemma not in dict:
-                dict[lemma] = []
-            else:
-                dict[lemma].append(i)
+def formalizeSearch(search):
+    result = []
+    morph = MorphAnalyzer()
+    words = search.split(" ")
+    for word in words:
+        elem = morph.normal_forms(word)[0]
+        result.append(elem)
+    return result
 
 
-def getAllFiles():
-    for i in range(101):
-        for filePath in glob.glob(path + str(i) + ".txt"):
-            for lemma in lemmas:
-                countAllLemmaApperances(filePath, lemma, i)
-                print(i)
+def searchAllLemmaApperances(search, tokens):
+    for elem in search:
+        for tokenStr in tokens:
+            token = morph.normal_forms(tokenStr[0])[0]
+            if token == elem:
+                if token not in dict:
+                    dict[elem] = []
+                    number = tokenStr[1].replace('\n', '')
+                    dict[elem].append(number)
+                else:
+                    number = tokenStr[1].replace('\n', '')
+                    dict[elem].append(number)
+            print(token)
 
 
-getAllFiles()
+def removeDuplicates():
+    for key in dict.keys():
+        dict[key] = list(dict.fromkeys(dict[key]))
+
+
+def boolSearch():
+    result = []
+    temp = list(dict)
+    size = len(dict)
+    for key in dict.keys():
+        if temp.index(key) < size-1:
+            nextKey = temp[temp.index(key) + 1]
+            result.append(set(dict[key]).intersection(dict[nextKey]))
+    return result
+
+result = formalizeSearch(search)
+searchAllLemmaApperances(result, tokens)
+removeDuplicates()
+boolSearch()
+
 print(dict)
